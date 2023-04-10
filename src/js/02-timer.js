2af8032578a079e174daf112a701a43baf2427d2
number-nine <sergii.pokhylko@gmail.com>
2023-04-10 17:27:58 +0300

    task2 beginning

---
+++ src/js/02-timer.js
@@ -0,0 +1,35 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+      initOnSuitableDate(selectedDates[0]);
+    },
+};
+
+const refs = {
+  dateSelectorEl: document.querySelector('#datetime-picker'),
+  startBtnEl: document.querySelector('[data-start]'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  secondsEl: document.querySelector('[data-seconds]'),
+};
+
+flatpickr(refs.dateSelectorEl, options);
+
+console.log(flatpickr.defaultDate);
+
+
+function initOnSuitableDate(date) {
+    if (Date.now() < date) {
+        refs.startBtnEl.disabled = false;
+        return;
+    }
+    refs.startBtnEl.disabled = true;
+    console.log('wrong date');
+}
